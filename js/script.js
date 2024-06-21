b114ee5c7f550094324213fb67e9da5fde8c5adb
hemant-kumawat-9192 <hemantkumawat99024@gmail.com>
2024-06-21 13:46:35 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function showmenu(){
+    $('#listmenu').toggleClass('showmenu');
+}
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
